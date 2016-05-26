66fb7cdebd6f611cc47644eb0136a5d6cf6747c1
Maritza <maritzaesparza05@gmail.com>
2016-05-26 13:50:53 -0500

    Styled forms

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
 	before_action :find_project, only: [:show, :edit, :update, :destroy]
-	efore_action :authenticate_user!, except: [:index, :show]
+	before_action :authenticate_user!, except: [:index, :show]
 	def index
 		@projects = Project.all.order("created_at desc")
 	end
